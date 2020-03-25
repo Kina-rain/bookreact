224e2d8fe2767a4057554d71b9ae536f45a3eb17
Kina Rain <tina@lawtk.net>
2020-03-24 22:18:53 -0400

    heroku again

--- server.js
+++ server.js
@@ -14,13 +14,15 @@ let app = express();
 app.use(express.urlencoded( { extended: true}) );
 app.use(express.json());
 
+console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // Handle React routing, return all requests to React app
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client','build','index.html'));
+  });  
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client', 'build')));  
 }
 
 //use the routes
