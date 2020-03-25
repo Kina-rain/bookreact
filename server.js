e74bd88fee19c19079c412a6637ab4a4605058b7
Kina Rain <tina@lawtk.net>
2020-03-25 08:07:07 -0400

    one more time

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === 'production') { 
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client', 'build')));  
+  app.use(express.static(path.join(__dirname, 'client')));  
 
   // Handle React routing, return all requests to React app
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client','build','index.html'));
+    res.sendFile(path.join(__dirname, 'client','public','index.html'));
   }); 
 }
 
